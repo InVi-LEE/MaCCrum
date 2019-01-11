11a8302ef6d638b580a1b102cc710a36c63b5182
Taesu Kim <ktaesu0821@gmail.com>
2019-01-11 12:00:05 +0900

    음성 인식 끈김없음

--- app/src/main/java/com/example/q/maccrum/MainActivity.java
+++ app/src/main/java/com/example/q/maccrum/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.q.maccrum;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
@@ -9,21 +10,32 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.vikramezhil.droidspeech.DroidSpeech;
+import com.vikramezhil.droidspeech.OnDSListener;
+import com.vikramezhil.droidspeech.OnDSPermissionsListener;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnDSListener, OnDSPermissionsListener {
 
+    private final String TAG = "mainactivity";
     Intent intent;
-    SpeechRecognizer mRecognizer;
-    ImageButton sttBtn;
+    DroidSpeech mDroidSpeech;
     TextView textView;
     final int PERMISSION = 1;
+    ImageButton start;
+    ImageButton stop;
+    static boolean first;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        first = true;
+        // Initialize Droid Speech
+        mDroidSpeech = new DroidSpeech(this, null);
+        mDroidSpeech.setOnDroidSpeechListener(this);
+        mDroidSpeech.setShowRecognitionProgressView(true);
 
         if ( Build.VERSION.SDK_INT >= 23 ){
             // 퍼미션 체크
@@ -40,98 +57,194 @@ public class MainActivity extends AppCompatActivity {
         }
 
         textView = (TextView)findViewById(R.id.sttResult);
-        sttBtn = (ImageButton) findViewById(R.id.sttStart);
 
         intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
-        sttBtn.setOnClickListener(v ->{
-            mRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
-            mRecognizer.setRecognitionListener(listener);
-            mRecognizer.startListening(intent);
+        intent.putExtra("android.speech.extra.DICTATION_MODE", true);
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, false);
+        start = findViewById(R.id.start);
+        start.setOnClickListener(this);
+
+        stop = findViewById(R.id.stop);
+        stop.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.start:
+
+                // Starting droid speech
+                mDroidSpeech.startDroidSpeechRecognition();
+
+                // Setting the view visibilities when droid speech is running
+                start.setVisibility(View.GONE);
+                stop.setVisibility(View.VISIBLE);
+
+                break;
+
+            case R.id.stop:
+
+                // Closing droid speech
+                mDroidSpeech.closeDroidSpeechOperations();
+
+                stop.setVisibility(View.GONE);
+                start.setVisibility(View.VISIBLE);
+
+                break;
+        }
+    }
+
+
+
+
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        if(stop.getVisibility() == View.VISIBLE)
+        {
+            stop.performClick();
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if(stop.getVisibility() == View.VISIBLE)
+        {
+            stop.performClick();
+        }
+    }
+    // MARK: OnClickListener Method
+
+
+
+    // MARK: DroidSpeechListener Methods
+
+    @Override
+    public void onDroidSpeechSupportedLanguages(String currentSpeechLanguage, List<String> supportedSpeechLanguages)
+    {
+        Log.i(TAG, "Current speech language = " + currentSpeechLanguage);
+//        Log.i(TAG, "Supported speech languages = " + supportedSpeechLanguages.toString());
+
+        if(supportedSpeechLanguages.contains("ko-KR"))
+        {
+            // Setting the droid speech preferred language as tamil if found
+            mDroidSpeech.setPreferredLanguage("ko-KR");
+
+            // Setting the confirm and retry text in tamil
+            mDroidSpeech.setOneStepVerifyConfirmText("시작되었습니다.");
+            mDroidSpeech.setOneStepVerifyRetryText("시작되었습니다.");
+        }
+    }
+
+    @Override
+    public void onDroidSpeechRmsChanged(float rmsChangedValue)
+    {
+        // Log.i(TAG, "Rms change value = " + rmsChangedValue);
+    }
+
+    @Override
+    public void onDroidSpeechLiveResult(String liveSpeechResult)
+    {
+        Log.i(TAG, "Live speech result = " + liveSpeechResult);
+    }
+
+    @Override
+    public void onDroidSpeechFinalResult(String finalSpeechResult)
+    {
+        // Setting the final speech result
+        if(first){
+            this.textView.setText(finalSpeechResult + "\n");
+            first = false;
+        }else{
+            this.textView.append(finalSpeechResult+"\n");
+        }
+
+        if(mDroidSpeech.getContinuousSpeechRecognition())
+        {
+            int[] colorPallets1 = new int[] {Color.RED, Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA};
+            int[] colorPallets2 = new int[] {Color.YELLOW, Color.RED, Color.CYAN, Color.BLUE, Color.GREEN};
+
+            // Setting random color pallets to the recognition progress view
+            mDroidSpeech.setRecognitionProgressViewColors(new Random().nextInt(2) == 0 ? colorPallets1 : colorPallets2);
+        }
+        else
+        {
+            stop.setVisibility(View.GONE);
+            start.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onDroidSpeechClosedByUser()
+    {
+        stop.setVisibility(View.GONE);
+        start.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onDroidSpeechError(String errorMsg)
+    {
+        // Speech error
+        Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
+
+        stop.post(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // Stop listening
+                stop.performClick();
+            }
         });
     }
 
-    private RecognitionListener listener = new RecognitionListener() {
-        @Override
-        public void onReadyForSpeech(Bundle params) {
-            Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+    // MARK: DroidSpeechPermissionsListener Method
+
+    @Override
+    public void onDroidSpeechAudioPermissionStatus(boolean audioPermissionGiven, String errorMsgIfAny)
+    {
+        if(audioPermissionGiven)
+        {
+            start.post(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    // Start listening
+                    start.performClick();
+                }
+            });
         }
-
-        @Override
-        public void onBeginningOfSpeech() {}
-
-        @Override
-        public void onRmsChanged(float rmsdB) {}
-
-        @Override
-        public void onBufferReceived(byte[] buffer) {}
-
-        @Override
-        public void onEndOfSpeech() {}
-
-        @Override
-        public void onError(int error) {
-            String message;
-
-            switch (error) {
-                case SpeechRecognizer.ERROR_AUDIO:
-                    message = "오디오 에러";
-                    break;
-                case SpeechRecognizer.ERROR_CLIENT:
-                    message = "클라이언트 에러";
-                    break;
-                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-                    message = "퍼미션 없음";
-                    break;
-                case SpeechRecognizer.ERROR_NETWORK:
-                    message = "네트워크 에러";
-                    break;
-                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
-                    message = "네트웍 타임아웃";
-                    break;
-                case SpeechRecognizer.ERROR_NO_MATCH:
-                    message = "찾을 수 없음";
-                    break;
-                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-                    message = "RECOGNIZER가 바쁨";
-                    break;
-                case SpeechRecognizer.ERROR_SERVER:
-                    message = "서버가 이상함";
-                    break;
-                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
-                    message = "말하는 시간초과";
-                    break;
-                default:
-                    message = "알 수 없는 오류임";
-                    break;
+        else
+        {
+            if(errorMsgIfAny != null)
+            {
+                // Permissions error
+                Toast.makeText(this, errorMsgIfAny, Toast.LENGTH_LONG).show();
             }
 
-            Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
+            stop.post(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    // Stop listening
+                    stop.performClick();
+                }
+            });
         }
+    }
 
-        @Override
-        public void onPause(){
-            MainActivity.super.onPause();
-            mRecognizer.destroy();
-        }
-
-        @Override
-        public void onResults(Bundle results) {
-            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
-            ArrayList<String> matches =
-                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-
-            for(int i = 0; i < matches.size() ; i++){
-                textView.setText(matches.get(i));
-            }
-        }
-
-        @Override
-        public void onPartialResults(Bundle partialResults) {}
-
-        @Override
-        public void onEvent(int eventType, Bundle params) {}
-    };
 }
 
